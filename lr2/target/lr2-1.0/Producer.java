e43bfd4f611f20d23570472bc3294c5504c77a54
dazzlemon <daanill13470@gmail.com>
2022-03-30 23:07:30 +0300

    much shorter thanks to jms 2.0

--- lr2/target/lr2-1.0/Producer.java
+++ lr2/target/lr2-1.0/Producer.java
@@ -1,44 +1,35 @@
 package com.lr2;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Session;
+import javax.jms.Queue;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import com.google.common.base.MoreObjects;
 
 public class Producer extends HttpServlet {
-	private Destination queue;
-	Connection initConnection() throws NamingException, JMSException {
-		var ic = new InitialContext();
-		var cf = (ConnectionFactory) ic.lookup("/ConnectionFactory");
-		queue = (Destination) ic.lookup("queue/tutorialQueue");
-		return cf.createConnection();
-	}
-
 	@Override
 	protected void doGet(
 		HttpServletRequest req, HttpServletResponse res
 	) throws ServletException, IOException {
 		var text = MoreObjects.firstNonNull(req.getParameter("text"), "Hello World");
 		var out = res.getWriter();
-		try (var connection = initConnection()) {
-			var session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			var publisher = session.createProducer(queue);
-			connection.start();
-			var message = session.createTextMessage(text);
-			publisher.send(message);
-			out.printf("Message sent: %s", text);
-		} catch (NamingException | JMSException e) {
-			out.printf("Error while trying to send <%s> message: %s", text, e.getMessage());
+		try {
+			var ic = new InitialContext();
+			var cf = (ConnectionFactory) ic.lookup("/ConnectionFactory");
+			var q = (Queue) ic.lookup("queue/tutorialQueue");
+			try (var context = cf.createContext()) {
+				context.createProducer().send(q, text);
+			}
+		} catch (Exception e) {
+			out.printf("Error while trying to send <%s> message: %s", text, e);
 		}
 	}
 }
