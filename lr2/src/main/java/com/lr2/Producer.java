6d2503522a704fdf88399b6282bc0786cfe64916
dazzlemon <daanill13470@gmail.com>
2022-03-30 21:04:50 +0300

    still working

--- lr2/src/main/java/com/lr2/Producer.java
+++ lr2/src/main/java/com/lr2/Producer.java
@@ -6,48 +6,41 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import com.google.common.base.MoreObjects;
+
 public class Producer extends HttpServlet {
 	private Destination queue;
 	Connection initConnection() throws NamingException, JMSException {
-		Context ic = new InitialContext();
-		ConnectionFactory cf = (ConnectionFactory) ic.lookup("/ConnectionFactory");
+		var ic = new InitialContext();
+		var cf = (ConnectionFactory) ic.lookup("/ConnectionFactory");
 		queue = (Destination) ic.lookup("queue/tutorialQueue");
 		return cf.createConnection();
 	}
 
 	@Override
 	protected void doGet(
-		HttpServletRequest req,
-		HttpServletResponse res
+		HttpServletRequest req, HttpServletResponse res
 	) throws ServletException, IOException {
-		res.setContentType("text/html");
+		var text = MoreObjects.firstNonNull(req.getParameter("text"), "Hello World");
 		var out = res.getWriter();
-		out.println("<html><body>");
-		out.println("Hello World!");
-		out.println("</body></html>");
-		out.close();
-
-		// String text = req.getParameter("text") != null ? req.getParameter("text") : "Hello World";
-		// try (Connection connection = initConnection()) {
-		// 	Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		// 	MessageProducer publisher = session.createProducer(queue);
-		// 	connection.start();
-		// 	TextMessage message = session.createTextMessage(text);
-		// 	publisher.send(message);
-		// } catch (NamingException | JMSException e) {
-		// 	res.getWriter().println("Error while trying to send <" + text + "> message: " + e.getMessage());
-		// }
-		// res.getWriter().println("Message sent: " + text);
+		try (var connection = initConnection()) {
+			var session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			var publisher = session.createProducer(queue);
+			connection.start();
+			var message = session.createTextMessage(text);
+			publisher.send(message);
+			out.printf("Message sent: %s", text);
+		} catch (NamingException | JMSException e) {
+			out.printf("Error while trying to send <%s> message: %s", text, e.getMessage());
+		}
 	}
 }
