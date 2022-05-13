cfb38e3a181426e5a142687b57760a3b0a135f2f
dazzlemon <daanill13470@gmail.com>
2022-05-13 21:00:44 +0300

    #5 done

--- lr5/src/java/lr5/CustomerController.java
+++ lr5/src/java/lr5/CustomerController.java
@@ -24,34 +24,23 @@ public class CustomerController extends HttpServlet {
         request.setAttribute("email",     email);
 
         var violations = new ArrayList<String>();
-        if (firstname.isBlank()) {
-            violations.add("First name required");
-        }
-        if (lastname.isBlank()) {
-            violations.add("Last name required");
-        }
-        if (email.isBlank()) {
-            violations.add("Email required");
-        }
+        if (firstname.isBlank()) violations.add("First name required");
+        if (lastname .isBlank()) violations.add("Last name required");
+        if (email    .isBlank()) violations.add("Email required");
         
         String regexPattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\."
                             + "[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@" 
                             + "[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
-        
-        if (Pattern.compile(regexPattern)
-                   .matcher(email)
-                   .matches()
-                   ) {
-            violations.add("Bad email format");
-        }
+        if (!Pattern.compile(regexPattern)
+                    .matcher(email)
+                    .matches()
+                    ) violations.add("Bad email format");
         
         String url;
-        if (!violations.isEmpty()) {
-            request.setAttribute("violations", violations);
-            url = "/";
-        } else {
-            url = "/WEB-INF/views/welcome.jsp";
-        }
+        if (!violations.isEmpty()) { request.setAttribute( "violations"
+                                                         , violations);
+                                     url = "/";
+        } else url = "/welcome.jsp";
         request.getRequestDispatcher(url).forward(request, response);
     }
 }
